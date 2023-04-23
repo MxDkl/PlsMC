1cc880090f6a4640928c855dc8c72537b27aa0d4
Max Dekel <max@dekelnet.com>
2023-04-23 08:43:59 -0400

    changed we prompt

--- app/worldedit.py
+++ app/worldedit.py
@@ -15,16 +15,18 @@ class WorldEdit(wrapper.openaiWrapper):
         I will provide documentation for some commands you may need to use.
         Manditory command arguments are surrounded by <>, and optional arguments are surrounded by [].
         Assume the user does not want a brush unless specified.
+        Some commands may require a region to be selected.
         Some keywords and their meanings:
         shape: the shape of the region; can be cuboid, cyl, sphere
         radius: the radius of the region; a number
         density: the density of the region; a number lower=more dense
         type: changes depending the command; can be block; biome; tree
+        pattern: the pattern of the region; can be block, biome, tree
         Here is the documentation:
         """
 
     def create(self):
-        res = self.search(self.prompt, "WorldEdit", 3)
+        res = self.search(self.prompt, "WorldEdit", 2)
         for i in res["matches"]:
             self.system_context += f"""
             Command: {i['metadata']['command']}
